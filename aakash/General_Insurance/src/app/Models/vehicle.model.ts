8115660c9f35ffb8c433faa92feb5d0d00f35766
aakashpatel98 <aakashptl98@gmail.com>
2022-03-19 19:42:08 +0530

    angular frnt end

---
+++ aakash/General_Insurance/src/app/Models/vehicle.model.ts
@@ -0,0 +1,12 @@
+export interface Vehicles
+{
+    twoWheeler: string;
+    fourWheeler: string;
+    Manufacturer :string;
+    Model : string;
+    DrivingLicenseNumber : string;
+    RegistrationNumber : string;
+    EngineNumber : string;
+    ChasisNumber : string;
+    PurchaseDate: Date;
+}
