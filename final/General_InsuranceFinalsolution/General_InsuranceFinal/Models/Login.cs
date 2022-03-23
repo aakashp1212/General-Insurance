94321208f5b64342f6e53296d32c0a257a0e1a5c
Mangala Vamsi Krishna <vamsi22122@gmail.com>
2022-03-24 01:57:01 +0530

    update

---
+++ final/General_InsuranceFinalsolution/General_InsuranceFinal/Models/Login.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace General_InsuranceFinal.Models
+{
+    public partial class Login
+    {
+        public int? UserId { get; set; }
+        public string Password { get; set; }
+        public int LoginId { get; set; }
+
+        public virtual CustomerRegistration User { get; set; }
+    }
+}
---
+++ final/General_InsuranceFinalsolution/General_InsuranceFinal/Models/Vehiclesdetails.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace General_InsuranceFinal.Models
+{
+    public partial class Vehiclesdetails
+    {
+        public string Manufacturer { get; set; }
+        public int? CustomerId { get; set; }
+        public int? VehicleTypeId { get; set; }
+        public string Model { get; set; }
+        public string DrivingLicenseNumber { get; set; }
+        public string RegistrationNumber { get; set; }
+        public string EngineNumber { get; set; }
+        public string ChassisNumber { get; set; }
+        public int? VehicleAge { get; set; }
+
+        public virtual CustomerRegistration Customer { get; set; }
+        public virtual Vehicletypes VehicleType { get; set; }
+    }
+}
