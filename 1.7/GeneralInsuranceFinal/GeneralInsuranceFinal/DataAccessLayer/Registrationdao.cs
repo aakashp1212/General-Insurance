e175212ddf5330bfebaaf844c4b017727aea5001
aakashpatel98 <aakashptl98@gmail.com>
2022-03-24 13:30:30 +0530

    new

---
+++ 1.7/GeneralInsuranceFinal/GeneralInsuranceFinal/DataAccessLayer/Registrationdao.cs
@@ -0,0 +1,41 @@
+﻿using GeneralInsuranceFinal.Interfaces;
+using GeneralInsuranceFinal.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace GeneralInsuranceFinal.DataAccessLayer
+{
+   
+        public class Registrationdao : IRegistration
+        {
+            private General_InsuranceDatabaseContext db;
+
+
+            public Registrationdao(General_InsuranceDatabaseContext _db)
+            {
+                db = _db;
+            }
+
+            public int AddCustomer(CustomerRegistration customer)
+            {
+                db.CustomerRegistration.Add(customer);
+                return db.SaveChanges();
+            }
+
+
+
+            public List<CustomerRegistration> GetALLCustomer()
+            {
+                return db.CustomerRegistration.ToList<CustomerRegistration>();
+
+            }
+
+            public CustomerRegistration GetCustomerById(long id)
+            {
+                return db.CustomerRegistration.Where(p => p.CustomerId == id).First<CustomerRegistration>();
+            }
+
+
+        }
+    
+}
---
+++ 1.7/GeneralInsuranceFinal/GeneralInsuranceFinal/Interfaces/IRenew.cs
@@ -0,0 +1,13 @@
+﻿namespace GeneralInsuranceFinal.Interfaces
+{
+    public interface IRenew
+    {
+
+        //List<CustomerRegistration> GetALLCustomer();
+
+        //int AddCustomer(CustomerRegistration customer);
+
+        //CustomerRegistration GetCustomerById(long CustomerId);
+
+    }
+}
