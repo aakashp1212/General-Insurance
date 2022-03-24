48356178d5c31f3722e6995234a9e7204aa65cab
Sindhuredddy-git <saisri9902@gmail.com>
2022-03-24 19:27:21 +0530

    FINAL DRAFT

---
+++ FINAL DRAFT/1.7/GeneralInsuranceFinal/GeneralInsuranceFinal/Interfaces/IBuyinsurance.cs
@@ -0,0 +1,28 @@
+﻿using GeneralInsuranceFinal.Models;
+using System.Collections.Generic;
+
+namespace GeneralInsuranceFinal.Interfaces
+{
+    public interface IBuyinsurance
+    {
+        
+
+
+            int AddInsurance(Vehiclesdetails vehiclesdetails);
+
+            int AddPolicy(Policydetails policydetails);
+
+
+
+            List<Vehicletypes> GetVehicletypes();
+
+            Vehiclesdetails GetInsuranceById(int id);
+
+            List<Insuranceplantype> GetPlans();
+
+            List<Insuranceduration> GetDuration();
+
+        
+
+    }
+}
---
+++ FINAL DRAFT/1.7/GeneralInsuranceFinal/GeneralInsuranceFinal/Models/Login.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+
+// Code scaffolded by EF Core assumes nullable reference types (NRTs) are not used or disabled.
+// If you have enabled NRTs for your project, then un-comment the following line:
+// #nullable disable
+
+namespace GeneralInsuranceFinal.Models
+{
+    public partial class Login
+    {
+        public Login()
+        {
+            CustomerRegistration = new HashSet<CustomerRegistration>();
+        }
+
+        public string Email { get; set; }
+        public string Password { get; set; }
+
+        public virtual ICollection<CustomerRegistration> CustomerRegistration { get; set; }
+    }
+}
